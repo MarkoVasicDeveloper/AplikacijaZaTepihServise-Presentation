a8d15d619c410b1e3b7ea11a64da4fe3f12e9b2e
Marko Vasic <perionicavasic2018@gmail.com>
2022-09-07 16:51:03 +0200

    Reademe

--- src/Components/LetsStart/LetsStart.tsx
+++ src/Components/LetsStart/LetsStart.tsx
@@ -63,8 +63,8 @@ export default function LetsStart() {
                 Na levoj strani se popunjavaju podaci o klijentu. Tepisi i staze
                 se pisu u komadima, Na strani za pripremu se popunjavaju
                 dimenzije za svaki tepih. Kada kliknete na posalji popunice Vam
-                se desna strana. Najvazniji podatak je{" "}
-                <span>ID broj klijenta</span>. To je broj kojim obelezavate
+                se desna strana. Najvazniji podatak je
+                <span> ID broj klijenta</span>. To je broj kojim obelezavate
                 tepihe. U donjem desnom uglu se nalaze podaci o posetama
                 klijenta Vasem servisu.
               </p>
@@ -83,7 +83,7 @@ export default function LetsStart() {
                 Na ovoj strani je potrebno da unesete ID broj kojim su obelezeni
                 tepisi. Klikom na posalji popunice Vam se podaci na levoj
                 strani. Bitno je da izaberete
-                <span>datum isporuke tepiha</span>, kako bi softver znao kada je
+                <span> datum isporuke tepiha</span>, kako bi softver znao kada je
                 isporuka. Na desnoj strani su Vam se pojavila polja za unos
                 dimenzija tepiha. Popunjavanjem dimenzija i njihovim slanjem, na
                 levoj strani ce vam biti izracunata suma za naplatu.
@@ -108,7 +108,7 @@ export default function LetsStart() {
                 tepiha navedete datum isporuke. Ukoliko ne mozete da nadjete
                 klijenta, imate dugme koje ce vam prikazati mapu. Kada zavrsite
                 izporuku kliknite na dugme
-                <span>isporuka savrsena</span>.
+                <span> isporuka savrsena</span>.
               </p>
             </div>
             <div className="explanation-delivery-list-img">
@@ -139,7 +139,7 @@ export default function LetsStart() {
               <p>
                 Sva zakazana preuzimanja ce biti prikazana. Takodje imate opciju
                 pomoci ukoliko ne mozete da nadjete klijenta. Kada zavrsite
-                preuzimanje kliknite na <span>preuzimanje zavrseno</span>.
+                preuzimanje kliknite na <span> preuzimanje zavrseno</span>.
               </p>
             </div>
             <div className="explanation-download-list-img">
